03993930c19092f1c81361b306bf61d83a3eaa20
Fahim Muntashir <fahimasdf07@gmail.com>
2025-12-30 17:56:22 +0600

    authorization sequrity

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,21 +1,8 @@
 import express, { NextFunction, Request, Response, Router } from "express";
 import { PostController } from "./post.controller";
 const router = express.Router();
-import { auth as betterAuth } from "../../lib/auth";
+import { auth, UserRole } from "../../middlewares/auth";
 
-const auth = (...roles: any) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    console.log("middle ware");
-
-    const session = await betterAuth.api.getSession({
-      headers: req.headers as any,
-    });
-
-    console.log(session);
-    next();
-  };
-};
-
-router.post("/", auth("ADMIN", "USER"), PostController.createPost);
+router.post("/", auth(UserRole.USER), PostController.createPost);
 
 export const postRouter: Router = router;
