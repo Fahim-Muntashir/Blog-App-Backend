609824fab53b7aaf6b5a1533f4338cc87cc288be
Fahim Muntashir <fahimasdf07@gmail.com>
2025-12-30 10:45:32 +0600

    google and email login

--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,7 +1,21 @@
-import express, { Router } from "express";
+import express, { NextFunction, Request, Response, Router } from "express";
 import { PostController } from "./post.controller";
 const router = express.Router();
+import { auth as betterAuth } from "../../lib/auth";
 
-router.post("/", PostController.createPost);
+const auth = (...roles: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    console.log("middle ware");
+
+    const session = await betterAuth.api.getSession({
+      headers: req.headers as any,
+    });
+
+    console.log(session);
+    next();
+  };
+};
+
+router.post("/", auth("ADMIN", "USER"), PostController.createPost);
 
 export const postRouter: Router = router;
--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 3000;
 async function main() {
   try {
     await prisma.$connect();
-    console.log("Connected to the database Successfully.");
+    console.log("Connected to the database successfully.");
 
     app.listen(PORT, () => {
-      console.log("Server is Running on ", PORT);
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.log(error);
+    console.error("An error occurred:", error);
     await prisma.$disconnect();
     process.exit(1);
   }
